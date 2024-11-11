b8c31fa07c4018e6e9d6ad02a71708b09c57644f
harry_arbrebleu <aoki.2.71828@keio.jp>
2024-11-12 01:53:36 +0900

    fixed 2nd order perturbation

--- compile_all.sh
+++ compile_all.sh
@@ -5,7 +5,7 @@ target_dir="."
 
 # 再帰的に処理する
 find "$target_dir" -type f -name "*.tex" | while read -r file; do
-    lualatex $file
     echo "Processing file: $file"
+    lualatex $file
     # ここにファイルへの処理を記述
 done
