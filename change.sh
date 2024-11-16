14e0edeb0ac07d24d0f5ac340a802364b6ef3128
harry_arbrebleu <aoki.2.71828@keio.jp>
2024-11-16 18:41:29 +0900

    finished electromagnetic filed

--- change.sh
+++ change.sh
@@ -9,11 +9,17 @@ find "$target_dir" -type f -name "main.tex" | while read -r filepath; do
     echo "Processing file: $filepath"
     
     # `Hamiltonian` を `ハミルトニアン` に置き換え
-    if grep -q "Hamiltonian" "$filepath"; then
-        sed -i 's/Hamiltonian/ハミルトニアン/g' "$filepath"
-        echo "Replaced 'Hamiltonian' with 'ハミルトニアン' in $filepath"
+    if grep -q "\begin{equation}" "$filepath"; then
+        sed -i 's/\begin{equation}/\begin{align}/g' "$filepath"
+        sed -i 's/\end{equation}/\end{align}/g' "$filepath"
+        # echo "Replaced 'Hamiltonian' with 'ハミルトニアン' in $filepath"
     else
-        echo "'Hamiltonian' not found in $filepath"
+        echo "'\begin{equation}' not found in $filepath"
+    fi
+    if grep -q "\mathrm{e}" "$filepath"; then
+        sed -i 's/\mathrm{e}/\e/g' "$filepath"
+    else
+        echo "'\begin{equation}' not found in $filepath"
     fi
 done
 
